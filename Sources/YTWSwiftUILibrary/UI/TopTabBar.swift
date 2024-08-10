382290fc2f3cd68d8258cd39b1f3631988445c9d
happyytw <happyytw@gmail.com>
2024-08-10 12:30:08 +0900

    updated on TopTabBar with Component to make more dynamic

--- Sources/YTWSwiftUILibrary/UI/TopTabBar.swift
+++ Sources/YTWSwiftUILibrary/UI/TopTabBar.swift
@@ -7,31 +7,72 @@
 
 import SwiftUI
 
+
+@available(iOS 17.0, *)
+public class UnderLineComponent {
+    var visible: Bool
+    var color: Color
+    var thickness: CGFloat
+    
+    init(visible: Bool = true, color: Color = Color.black, thickness: CGFloat = 1.0) {
+        self.visible = visible
+        self.color = color
+        self.thickness = thickness
+    }
+}
+
+@available(iOS 17.0, *)
+public class TextComponent {
+    var tabs: [String]
+    var selectedColor: Color
+    var unselectedColor: Color
+    var isBold: Bool
+    
+    init(tabs: [String], selectedColor: Color = Color.black, unselectedColor: Color = Color.gray, isBold: Bool = true) {
+        self.tabs = tabs
+        self.selectedColor = selectedColor
+        self.unselectedColor = unselectedColor
+        self.isBold = isBold
+    }
+}
+
+
 @available(iOS 17.0, *)
 public struct TopTabBar<Content: View>: View {
     
     let contents: Content
-    private var tabs: [String] = []
+    let textComponent: TextComponent
+    let underlineComponent: UnderLineComponent
     
     @State private var selectedTab = 0
     @State private var verticalOffset: CGFloat = 0
     
-    
-    public init(@ViewBuilder content: () -> Content, arr: [String]) {
+    public init(@ViewBuilder content: () -> Content, text: TextComponent, underline: UnderLineComponent) {
         self.contents = content()
-        self.tabs = arr
+        self.textComponent = TextComponent(tabs: ["자산", "소비﹒수입", "연말정산"], selectedColor: Color.red, unselectedColor: Color.gray, isBold: true)
+        self.underlineComponent = UnderLineComponent(visible: true, color: Color.black, thickness: 1.0)
     }
     
     public var body: some View {
         VStack {
             HStack {
-                ForEach(Array(tabs.enumerated()), id: \.offset) { obj in
+                ForEach(Array(textComponent.tabs.enumerated()), id: \.offset) { obj in
                     Button {
                         selectedTab = obj.offset
                         print(selectedTab)
                     } label: {
-                        Text(obj.element)
-                            .foregroundStyle(obj.offset == selectedTab ? Color.accentColor : Color.gray)
+                        VStack {
+                            Text(obj.element)
+                                .bold(selectedTab == obj.offset)
+                                .foregroundStyle(obj.offset == selectedTab ? textComponent.selectedColor : textComponent.unselectedColor)
+                                .overlay {
+                                    if underlineComponent.visible && selectedTab == obj.offset {
+                                        underlineComponent.color
+                                            .frame(width: UIScreen.main.bounds.width/3, height: underlineComponent.thickness)
+                                            .offset(x: -(verticalOffset/3), y: 20)
+                                    }
+                                }
+                        }
                     }
                     .frame(maxWidth: .infinity)
                 }
@@ -41,7 +82,7 @@ public struct TopTabBar<Content: View>: View {
             
             GeometryReader { geometry in
                 HStack(spacing: 0) {
-                    ForEach(0..<tabs.count, id: \.self) { index in
+                    ForEach(0..<textComponent.tabs.count, id: \.self) { index in
                         contents
                             .frame(width: geometry.size.width, height: geometry.size.height)
                     }
@@ -56,7 +97,7 @@ public struct TopTabBar<Content: View>: View {
                         }
                         .onEnded { value in
                             let threshold = geometry.size.width / 2
-                            if -value.predictedEndTranslation.width > threshold && selectedTab < tabs.count - 1 {
+                            if -value.predictedEndTranslation.width > threshold && selectedTab < textComponent.tabs.count - 1 {
                                 selectedTab += 1
                             } else if value.predictedEndTranslation.width > threshold && selectedTab > 0 {
                                 selectedTab -= 1
@@ -66,6 +107,7 @@ public struct TopTabBar<Content: View>: View {
                 )
             }
         }
+        
     }
 }
 
@@ -75,10 +117,20 @@ public struct TopTabBar<Content: View>: View {
 @available(iOS 17.0, *)
 #Preview {
     TopTabBar(content: {
-                Text("First View")
-                Text("Second View")
-                Text("Third View")
-            }, arr: ["자산", "소비﹒수입", "연말정산"])
+        Text("First View")
+        Text("Second View")
+        Text("Third View")
+    }, text: TextComponent(tabs: ["자산", "소비﹒수입", "연말정산"]), underline: UnderLineComponent())
+//    TopTabBar(content: {
+//                Text("First View")
+//                Text("Second View")
+//                Text("Third View")
+//            }, 
+//              arr: ["자산", "소비﹒수입", "연말정산"],
+//              unselectColor: Color.gray,
+//              selectColor: Color.red,
+//              underline: UnderLineComponent()
+//    )
 }
 
 @available(iOS 17.0, *)
